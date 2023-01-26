2c7c76b800b7e3d81ad704411a6a38498bed3150
Sergey <sergey150465@mail.ru>
2023-01-26 10:27:18 +0300

    Initial commit

---
+++ src/main/java/GetCatInformation.java
@@ -0,0 +1,44 @@
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.entity.ContentType;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
+import java.util.List;
+
+public class GetCatInformation {
+    public static final ObjectMapper mapper = new ObjectMapper();
+    public static final String REMOTE_SERVICE_URI = "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats";
+
+    public static void main(String[] args) throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)    // максимальное время ожидание подключения к серверу
+                        .setSocketTimeout(30000)    // максимальное время ожидания получения данных
+                        .setRedirectsEnabled(false) // возможность следовать редиректу в ответе
+                        .build())
+                .build();
+
+        HttpGet request = new HttpGet(REMOTE_SERVICE_URI);
+        request.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
+
+        CloseableHttpResponse response = httpClient.execute(request);
+
+        List<Post> posts = mapper.readValue(
+                response.getEntity().getContent(),
+                new TypeReference<>() {
+                });
+
+        posts.stream()
+                .filter(value -> Integer.toString(value.getUpvotes()) != null && value.getUpvotes() > 0)
+                .forEach(System.out::println);
+
+        response.close();
+        httpClient.close();
+    }
+}
